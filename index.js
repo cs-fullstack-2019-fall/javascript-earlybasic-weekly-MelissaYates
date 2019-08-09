23a744934d481a993130a028db033ae19f1b61d3
Melissa Yates <melissampeart@gmail.com>
2019-08-09 15:59:33 -0500

    practice project sigh

---
+++ index.js
@@ -0,0 +1,42 @@
+
+var userEntry = parseInt(prompt("Welcome to The Tutorial. Please enter option 1 to count or option 2 to add/subtract. Enter q to quit."));
+if(userEntry===1 ||userEntry===2){
+	while(userEntry===1 || userEntry===2){
+		var x =0;
+		if(userEntry===1)
+				{
+					var userEntry1= parseInt(prompt("Please enter a number. Please count by ones. "));
+				for(x =1; x<=userEntry; x++)
+
+					{
+
+						userEntry1 =1;
+						console.log(userEntry1);
+						userEntry1++;
+					}
+				}
+		else(userEntry===2)
+				{
+					var userEntryA = prompt("Do you want to add or subtract?(type 'add' to do add.")
+					var add=0;
+					var userEntry1 = parseInt(prompt("Please enter a number"));
+					var userEntry2 = parseInt(prompt("Please enter a second number"));
+					if(userEntryA===add)
+						{
+					for(var x = userEntry1; x<=userEntry2; x++)
+						{
+					console.log(summ= userEntry1 + userEntry2);
+					alert(summ);
+						}
+						}
+}
+}
+else{
+		prompt("Enter q to quit");
+	}
+
+				}
+	
+
+		
+	
